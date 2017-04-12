d3b678009490c91d02bdd828d834fd7b0cf094c7
xcvJake <jake.wood@my.jcu.edu.au>
2017-04-12 14:27:09 +1000

    Objects rotate around the player, however, the translation is unbounded. And will therefore need to be bounded.

--- CP3311_Assignment/Assets/Scripts/PlayerController.cs
+++ CP3311_Assignment/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed = 6f;            // The speed that the player will move at.
+    public int junkRotationSpeed = 500;
 
     Vector3 movement;                   // The vector to store the direction of the player's movement.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    GameObject[] floatingJunk;
+    int junkIndex;
+    
+
+    private void Start()
+    {
+        floatingJunk = new GameObject[10];
+        junkIndex = 0; 
+    }
 
     void Awake()
     {
@@ -24,6 +34,8 @@ public class PlayerController : MonoBehaviour {
         // Move the player around the scene.
         Move(h, v);
 
+        // Rotate the Junk around the player
+        rotateJunk();
     }
 
     void Move(float h, float v)
@@ -42,7 +54,54 @@ public class PlayerController : MonoBehaviour {
     {
         if (other.tag == "Building")
         {
-            other.gameObject.SetActive(false);
+            other.transform.parent = transform;
+            //transform.parent = other.transform;
+
+            if(floatingJunk[junkIndex] != null)
+            {
+                // Could also delete object
+                floatingJunk[junkIndex].gameObject.SetActive(false);
+
+                // Add the new piece of junk to the array
+                floatingJunk[junkIndex] = other.gameObject;
+            }
+
+            if (junkIndex >= 9)
+            {
+                junkIndex = 0;
+            }
+            else
+            {
+                junkIndex++;
+            }
+            
+
+        
+            //other.gameObject.SetActive(false);
         }
+    }
+
+    private void rotateJunk()
+    {
+        foreach (Transform child in transform)
+        {
+            //child is your child transform
+            if (child.gameObject.tag == "Building")
+            {
+
+                child.RotateAround(transform.position, new Vector3(0, 3, 0), junkRotationSpeed*Time.deltaTime);
+
+                child.Translate(new Vector3(0, 0.1f, 0));
+                
+            }
+        }
+
+        //foreach (GameObject junk in floatingJunk)
+        //{
+        //    if (junk != null)
+        //    {
+        //        junk.transform.RotateAround(Vector3.zero, Vector3.up, 20);
+        //    }
+        //}
     }
 }
