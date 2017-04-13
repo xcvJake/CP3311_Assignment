c3cb2691fe376bcde6bc23c7e1a6de0ae29b30a0
xcvJake <jake.wood@my.jcu.edu.au>
2017-04-13 18:50:39 +1000

    + Rotation is working, having issues with junk flying off due to translation speed. - removed rigidbody from junk, now can make huge tentacle of objects

--- CP3311_Assignment/Assets/Scripts/PlayerController.cs
+++ CP3311_Assignment/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 6f;            // The speed that the player will move at.
-    public int junkRotationSpeed = 500;
+    public float speed = 10f;            // The speed that the player will move at.
+    public int junkRotationSpeed = 250;
+    public float junkTranslateSpeed = 0.01f;
+    public float junkHeightMax = 6f;
+    public float junkHeightMin = 3f; 
 
     Vector3 movement;                   // The vector to store the direction of the player's movement.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-    GameObject[] floatingJunk;
-    int junkIndex;
-    
+    float[] junkHeightArray;
+    int[] junktranslateDirectionArray;
+    int junkArrayIndex;
+
+    Transform orbitingJunk;
+
+    public int junkCount = 10;
+
 
     private void Start()
     {
-        floatingJunk = new GameObject[10];
-        junkIndex = 0; 
+        junkHeightArray = new float[10];
+        junktranslateDirectionArray = new int[10];
+        junkArrayIndex = 0;
+        orbitingJunk = transform.FindChild("OrbitingJunk");
     }
 
     void Awake()
@@ -54,25 +64,53 @@ public class PlayerController : MonoBehaviour {
     {
         if (other.tag == "Building")
         {
-            other.transform.parent = transform;
-            //transform.parent = other.transform;
 
-            if(floatingJunk[junkIndex] != null)
+
+            // delete junk
+            if (orbitingJunk.transform.childCount >= junkCount)
             {
-                // Could also delete object
-                floatingJunk[junkIndex].gameObject.SetActive(false);
-
-                // Add the new piece of junk to the array
-                floatingJunk[junkIndex] = other.gameObject;
+                Destroy(orbitingJunk.GetChild(0).gameObject);
             }
 
-            if (junkIndex >= 9)
+
+
+
+            other.transform.parent = orbitingJunk.transform; 
+            //other.isTrigger = false;
+            //other.attachedRigidbody.detectCollisions = false;
+            //other.attachedRigidbody.isKinematic = false;
+            //other.attachedRigidbody.useGravity = false;
+
+
+
+
+
+
+
+
+
+            //transform.parent = other.transform;
+
+            //if(floatingJunk[junkIndex] != null)
+            //{
+            //    // Could also delete object
+            //    floatingJunk[junkIndex].gameObject.SetActive(false);
+
+            //    // Add the new piece of junk to the array
+            //    floatingJunk[junkIndex] = other.gameObject;
+
+            //}
+
+            junkHeightArray[junkArrayIndex] = other.transform.position.y; // starts at where it is
+            junktranslateDirectionArray[junkArrayIndex] = 1; // Up by default
+            
+            if (junkArrayIndex >= 9)
             {
-                junkIndex = 0;
+                junkArrayIndex = 0;
             }
             else
             {
-                junkIndex++;
+                junkArrayIndex++;
             }
             
 
@@ -83,25 +121,42 @@ public class PlayerController : MonoBehaviour {
 
     private void rotateJunk()
     {
-        foreach (Transform child in transform)
+        int junkPieceIndex = 0; 
+        foreach (Transform child in orbitingJunk.transform)
         {
             //child is your child transform
             if (child.gameObject.tag == "Building")
             {
+                // Rotates Child Junk Piece around Cyclone
+                child.Rotate(new Vector3(0.5f, 0.2f, 0.2f));
+                child.RotateAround(transform.position, new Vector3(0, 1, 0), junkRotationSpeed*Time.deltaTime);
 
-                child.RotateAround(transform.position, new Vector3(0, 3, 0), junkRotationSpeed*Time.deltaTime);
+                // Translate the Junk Piece in a sin/oscillation up and down
 
-                child.Translate(new Vector3(0, 0.1f, 0));
-                
+                // Is the junk already above the maximum? 
+                if(junkHeightArray[junkPieceIndex] >= junkHeightMax)
+                {
+                    // junk go down
+                    junktranslateDirectionArray[junkPieceIndex] = -1; 
+                }
+                else if (junkHeightArray[junkPieceIndex] <= junkHeightMin)
+                {
+                    // junk go up
+                    junktranslateDirectionArray[junkPieceIndex] = 1;
+                }
+
+                // Increase/Decrease the junk height based on direction
+                //junkHeightArray[junkPieceIndex] += (junktranslateDirectionArray[junkPieceIndex] * junkHeightSine);
+
+                // Move the junk to the new location
+                //child.Translate(new Vector3(0, Mathf.Sin(junkHeightArray[junkPieceIndex])/10, 0));
+                child.Translate(new Vector3(0, (junktranslateDirectionArray[junkPieceIndex] * junkTranslateSpeed), 0));
+
+                junkHeightArray[junkPieceIndex] = child.transform.position.y;
+
+                // next junk piece in array please
+                junkPieceIndex++;
             }
         }
-
-        //foreach (GameObject junk in floatingJunk)
-        //{
-        //    if (junk != null)
-        //    {
-        //        junk.transform.RotateAround(Vector3.zero, Vector3.up, 20);
-        //    }
-        //}
     }
 }
--- CP3311_Assignment/Assets/Scripts/RotationController.cs
+++ CP3311_Assignment/Assets/Scripts/RotationController.cs
@@ -6,35 +6,12 @@ public class RotationController : MonoBehaviour {
 
     public GameObject player;
 
-    bool playerTrigger;
-
-
 	// Use this for initialization
 	void Start () {
-        playerTrigger = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Player")
-    //    {
-    //        playerTrigger = true;
-            
-    //    }
-    //}
-
-
-    //private void FixedUpdate()
-    //{
-    //    // If the player has triggered the object, start rotating around the player
-    //    if (playerTrigger)
-    //    {
-    //        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
-    //    }
-    //}
 }
