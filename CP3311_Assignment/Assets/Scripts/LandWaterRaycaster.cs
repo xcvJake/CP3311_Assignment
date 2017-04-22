cdd06297c706d90352991d6182ae81646e7e0013
Jack Newman <jack.newman@my.jcu.edu.au>
2017-04-22 18:09:03 +1000

    + Added Raycast to detect water, adds/removes health when appropriate

---
+++ CP3311_Assignment/Assets/Scripts/LandWaterRaycaster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class LandWaterRaycaster : MonoBehaviour {
+
+	public float Health = 100;
+	public float MaxHealth = 100;
+	public float DamageRate = 1;
+	public float HealRate = 5;
+	private RaycastHit hitInfo;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Physics.Raycast(transform.position, -transform.up, out hitInfo , 10)){
+			if (hitInfo.transform.tag == "Water") {
+				Health += HealRate * Time.deltaTime;
+			}
+			else {
+				Health += -1 * DamageRate * Time.deltaTime;
+			}
+
+
+			if (Health > MaxHealth) {
+				Health = MaxHealth;
+			}
+		}
+	}
+}
