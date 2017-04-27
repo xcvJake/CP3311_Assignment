ce2c2835813c651a1977331b6ffee2fa31938723
Jack Newman <jack.newman@my.jcu.edu.au>
2017-04-27 10:37:27 +1000

    +Updated FPS Cyclone Junk Removal +Messed with world scaling a little

--- CP3311_Assignment/Assets/Scripts/PlayerControllerJunkRewrite.cs
+++ CP3311_Assignment/Assets/Scripts/PlayerControllerJunkRewrite.cs
@@ -2,87 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerJunkRewrite : MonoBehaviour {
+public class PlayerControllerJunkRewrite : MonoBehaviour
+{
 
 	public int junkRotationSpeed = 250;
-//	public float junkHeightMax = 6f;
+	//	public float junkHeightMax = 6f;
 	public float junkHeightMin = 1f;
 	public static float cycloneMassLimit = 1f;
-    // public float testingMassAdjust = 1f; //testing thing
+	// public float testingMassAdjust = 1f; //testing thing
 
 	Transform orbitingJunk;
 	CapsuleCollider cycloneCollider;
 
-	public int junkCount = 100;
+	//public int junkCount = 100;
 	public float junkPercentMultiplier = 0.1f;
 
 	public float turningSpeed = 100;
 	public float movementSpeed = 10;
 
 
-	private void Start()
+	private void Start ()
 	{
-		orbitingJunk = transform.FindChild("OrbitingJunk");
+		orbitingJunk = transform.FindChild ("OrbitingJunk");
 		cycloneCollider = transform.GetComponent<CapsuleCollider> ();
 	}
 
-	void Awake()
+	void Awake ()
 	{
 
 	}
 
-	void FixedUpdate()
+	void FixedUpdate ()
 	{
 		// Store the input axes.
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
 
 		// Move the player around the scene.
-		Move(h, v);
+		Move (h, v);
 
 		// Rotate the Junk around the player
-		rotateJunk();
-		spinJunk();
+		rotateJunk ();
+		spinJunk ();
 	}
 
-	void Update()
+	void Update ()
 	{
-        // Testing thing
-        //cycloneMassLimit = testingMassAdjust;
+		// Testing thing
+		//cycloneMassLimit = testingMassAdjust;
 
-        cleanupCyclone (); //This must be in Update if framerate depending cleanup is wanted
+		cleanupCyclone (); //This must be in Update if framerate depending cleanup is wanted
 	}
 
-	void cleanupCyclone(){
-		// delete junk. This CANT be a while, since childCount takes time to update, and by then objects have been deleted and unity freezes/crashes PC, ask me how I know
-//		if (orbitingJunk.transform.childCount >= junkCount)
-//		{
-//			Destroy(orbitingJunk.GetChild(0).gameObject);
-//		}
-		if (1.0f/Time.deltaTime < 30 && orbitingJunk.transform.childCount > 10) //If FPS < 30, start removing items. So higher end pcs can enjoy a large number of items
-		{
-			Destroy(orbitingJunk.GetChild(0).gameObject);
+	int numberOfFPSLoops = 0;
+
+	void cleanupCyclone ()
+	{
+
+		int FPSLimit = 20; //Note this is not real FPS, it includes drawing time and whatnot, so kinda hovers around half the fps
+
+		if (1.0f / Time.deltaTime < FPSLimit) { //If FPS < 30, start removing items. So higher end pcs can enjoy a large number of items
+			numberOfFPSLoops++;
+			if (numberOfFPSLoops > FPSLimit / 2) { //Make sure fps drop has been going on longer than half a second
+				
+				int childKillingPower = FPSLimit - (int)(1.0f / Time.deltaTime);
+				//childKillingPower = childKillingPower * childKillingPower; 	//EXPONENTIAL CHILD KILLING POWER WAHAHAHA 
+				childKillingPower = childKillingPower * 30; // Ok, note to self, too much child killing, lets tone it down. Murderer.
+				childKillingPower = childKillingPower > orbitingJunk.childCount ? orbitingJunk.childCount : childKillingPower;
+
+				for (int i = 0; i < childKillingPower; i++) {
+					Destroy (orbitingJunk.GetChild (i).gameObject);
+				}
+				numberOfFPSLoops = 0;
+			}
+
+		} else {
+			numberOfFPSLoops = 0;
+
 		}
 
 	}
 
-	void Move(float h, float v)
+	void Move (float h, float v)
 	{
 
 		float horizontal = h * turningSpeed * Time.deltaTime;
-		transform.Rotate(0, horizontal, 0);
+		transform.Rotate (0, horizontal, 0);
 
 		float verticalTranslate = v * movementSpeed * Time.deltaTime;
 		float horizontalTranslate = 0;
-		transform.Translate(horizontalTranslate, 0, verticalTranslate);
+		transform.Translate (horizontalTranslate, 0, verticalTranslate);
 
 
 	}
 
-	void OnTriggerEnter(Collider other)
+	void OnTriggerEnter (Collider other)
 	{
-		if ((other.tag == "pickup") && (other.attachedRigidbody.mass <= cycloneMassLimit))
-		{
+		if ((other.tag == "pickup") && (other.attachedRigidbody.mass <= cycloneMassLimit)) {
 			cycloneMassLimit += other.attachedRigidbody.mass * junkPercentMultiplier; 
 			//TODO: Increase size of collider, both width, height and Y offset based off cycloneMass (This will have to sync with increasing partical sizes or whatever)
 			//TODO: Increase size of actual cyclone 
@@ -93,25 +109,25 @@ public class PlayerControllerJunkRewrite : MonoBehaviour {
 			other.transform.parent = orbitingJunk.transform;
 
 			//Add a random height, makes the orbiting look a little nicer, from the ground to height of collider
-			Vector3 randomHeight = other.transform.position + (transform.up.normalized * Random.Range(junkHeightMin, cycloneCollider.height));
+			Vector3 randomHeight = other.transform.position + (transform.up.normalized * Random.Range (junkHeightMin, cycloneCollider.height));
 			other.transform.position = randomHeight;
 
 		}
 	}
 
 
-	private void spinJunk()
+	private void spinJunk ()
 	{
 		foreach (Transform child in orbitingJunk.transform) {
-			child.Rotate (Random.Range(0f, 5f), Random.Range(0f, 5f), Random.Range(0f, 5f));
+			child.Rotate (Random.Range (0f, 5f), Random.Range (0f, 5f), Random.Range (0f, 5f));
 
 		}
 	}
 
 
-	private void rotateJunk()
+	private void rotateJunk ()
 	{
 		Vector3 rotateDirection = new Vector3 (0f, 1f, 0f);
-		orbitingJunk.transform.Rotate(rotateDirection * junkRotationSpeed);
+		orbitingJunk.transform.Rotate (rotateDirection * junkRotationSpeed);
 	}
 }
--- CP3311_Assignment/Assets/Scripts/WorldScalingController.cs
+++ CP3311_Assignment/Assets/Scripts/WorldScalingController.cs
@@ -2,53 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldScalingController : MonoBehaviour {
+public class WorldScalingController : MonoBehaviour
+{
 
-    /*
+	//public float cycloneMaxSize = 10000;
+	//public float rescaleIncrement = 1;
+	//public float rescalePercent = 0.9f;
+	//float previousRescaleSize = 1;
+	//float cycloneMassLimit;
+
+
+	public Transform player;
+	public float scalingAmount = 0.1f;
+	//WARNING: This current value results in an exponential curve, not sure if this is what we wanted / warning
+	//Might want to replace this with some nice sort of curve, but is kinda fun
+	public float smallestScale = 0.1f;
+	float startingWeight;
+	Vector3 startingWorldScale;
+	void Start ()
+	{
+		startingWeight = PlayerControllerJunkRewrite.cycloneMassLimit; //Allows us to start with any mass and not scale the world on start
+		startingWorldScale = transform.localScale; //Allows us to start with the map at any scale
+	}
+
+
+	void Update ()
+	{
+		//Scale the world based on the mass of the cyclone.  
+		Vector3 newScale = startingWorldScale - (new Vector3 (scalingAmount, scalingAmount, scalingAmount) * (PlayerControllerJunkRewrite.cycloneMassLimit - startingWeight));
+
+		if (newScale.x > smallestScale) {
+			//Based on the scaling we will do, we will need to move the piviot point, so as to seem like we are scaling around a point
+			float RelativeScale = (newScale.x / transform.localScale.x);
+			if (RelativeScale != 1) {
+				Vector3 vectorBetweenEnvironmentAndPlayer = (transform.position - player.position);
+				transform.position = (vectorBetweenEnvironmentAndPlayer * RelativeScale) + player.position;
+			}
+			transform.localScale = newScale.x < smallestScale ? new Vector3 (smallestScale, smallestScale, smallestScale) : newScale;
+		} 
+
+		/* We could scale the objects that are currently inside of the cyclone, but I think the occasional silly large item looks really cool */
+
+	}
+}
+
+
+/*
      * 
      * /----------------------------------------\
-     * |       MAKE SURE THE ENVIRONMENT        |
-     * |            GAMEOBJECT IS               |
-     * |      AT WORLD CO-ORD: [0,0,0] !!!!     |
-     * \----------------------------------------/
-     *  why, because fuk u thats why.
+		* |       MAKE SURE THE ENVIRONMENT        |
+		* |            GAMEOBJECT IS               |
+		* |      AT WORLD CO-ORD: [0,0,0] !!!!     |
+		* \----------------------------------------/
+		*  why, because fuk u thats why.
      *  also because the cyclone repositioning moves the it towards origin. 
      */
 
-    public float cycloneMaxSize = 10000;
-    public float rescaleIncrement = 1;
-    public float rescalePercent = 0.9f;
-    float previousRescaleSize = 1;
-    float cycloneMassLimit;
-    public Transform player;
-
-    // Use this for initialization
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        cycloneMassLimit = PlayerControllerJunkRewrite.cycloneMassLimit;
-
-        if(cycloneMassLimit >= (previousRescaleSize + rescaleIncrement))
-        {
-            transform.localScale = transform.localScale * rescalePercent;
-            previousRescaleSize = cycloneMassLimit;
-
-            // move le cyclone because the world done shrinky
-            player.position = player.position * rescalePercent; 
-
-
-
-        } 
-        else if (cycloneMassLimit <= (previousRescaleSize - rescaleIncrement))
-        {
-            transform.localScale = transform.localScale * (2 - rescalePercent);
-            previousRescaleSize = cycloneMassLimit;
-
-            // move el cyclone because the world done a grow
-            player.position = player.position * (2 - rescalePercent);
-
-        }
-    }
-}
+//        cycloneMassLimit = PlayerControllerJunkRewrite.cycloneMassLimit;
+//
+//        if(cycloneMassLimit >= (previousRescaleSize + rescaleIncrement))
+//        {
+//            transform.localScale = transform.localScale * rescalePercent;
+//            previousRescaleSize = cycloneMassLimit;
+//
+//            // move le cyclone because the world done shrinky
+//            player.position = player.position * rescalePercent;
+//
+//
+//
+//        }
+//        else if (cycloneMassLimit <= (previousRescaleSize - rescaleIncrement))
+//        {
+//            transform.localScale = transform.localScale * (2 - rescalePercent);
+//            previousRescaleSize = cycloneMassLimit;
+//
+//            // move el cyclone because the world done a grow
+//            player.position = player.position * (2 - rescalePercent);
+//
+//        }
