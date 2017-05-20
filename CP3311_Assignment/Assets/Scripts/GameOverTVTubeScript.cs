e000b8db6d4e042435bce9ac2c9e5712635e6ad9
xcvJake <jake.wood@my.jcu.edu.au>
2017-05-20 22:06:00 +1000

    Added the tube tv turn off at the end of the video

--- CP3311_Assignment/Assets/Scripts/GameOverTVTubeScript.cs
+++ CP3311_Assignment/Assets/Scripts/GameOverTVTubeScript.cs
@@ -1,59 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video; 
 
 public class GameOverTVTubeScript : MonoBehaviour {
+
+	VideoPlayer videoPlayer; 
+	bool videoPlayed;
+	public float playerMassLimit = 100f;
+
 	void Start()
 	{
-//		// Will attach a VideoPlayer to the main camera.
-//		GameObject camera = GameObject.Find("Main Camera");
-//
-//		// VideoPlayer automatically targets the camera backplane when it is added
-//		// to a camera object, no need to change videoPlayer.targetCamera.
-//		var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
-//
-//		// By default, VideoPlayers added to a camera will use the far plane.
-//		// Let's target the near plane instead.
-//		videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
-//
-//		// This will cause our scene to be visible through the video being played.
-//		videoPlayer.targetCameraAlpha = 0.5F;
-//
-//		// Set the video to play. URL supports local absolute or relative paths.
-//		// Here, using absolute.
-//		videoPlayer.url = "/Users/graham/movie.mov";
-//
-//		// Skip the first 100 frames.
-//		videoPlayer.frame = 100;
-//
-//		// Restart from beginning when done.
-//		videoPlayer.isLooping = true;
-//
-//		// Each time we reach the end, we slow down the playback by a factor of 10.
-//		videoPlayer.loopPointReached += EndReached;
-
-		// Start playback. This means the VideoPlayer may have to prepare (reserve
-		// resources, pre-load a few frames, etc.). To better control the delays
-		// associated with this preparation one can use videoPlayer.Prepare() along with
-		// its prepareCompleted event.
+		videoPlayer = gameObject.GetComponent<VideoPlayer>();
+		videoPlayer.isLooping = false;
+		videoPlayed = false;
 	}
 
-//	void AnimatorUpdateMode () {
-//
-//
-//
-//
-//
-//		if(false) {
-//			videoPlayer.Play();
-//		}
-//	}
-//
-//
-//
-//
-//	void EndReached(UnityEngine.Video.VideoPlayer vp)
-//	{
-//		vp.playbackSpeed = vp.playbackSpeed / 10.0F;
-//	}
+	void Update () {
+		if (PlayerControllerJunkRewrite.cycloneMassLimit > playerMassLimit && !videoPlayed) {
+			videoPlayed = true;
+			videoPlayer.Play ();
+		} else if (videoPlayed && !videoPlayer.isPlaying) {
+			
+			// Game over screen goes here.
+			//TODO: 
+		}
+	}
 }
